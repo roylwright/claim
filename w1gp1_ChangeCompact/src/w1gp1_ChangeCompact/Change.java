9a5fae77d367a657f4c5b769fa72b15d72ea0821
RoyLWright <roylwright@gmail.com>
2017-08-28 18:24:51 -0500

    removed blanks added comments

--- w1gp1_ChangeCompact/src/w1gp1_ChangeCompact/Change.java
+++ w1gp1_ChangeCompact/src/w1gp1_ChangeCompact/Change.java
@@ -2,18 +2,25 @@ package w1gp1_ChangeCompact;
 import java.util.Scanner;
 public class Change {	
 	private static int userInput;
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
+		//scanner obj enters integer value for conversion to change values
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Enter a value between 0 and 100: ");
 		userInput = scanner.nextInt();
-		scanner.close();		
+		scanner.close();
+		//calls method for number of coin required, returns undetermined change
 		userInput = change("Quarters: ", 25, userInput);
 		userInput = change("Dimes: ", 10, userInput);
 		userInput = change("Nickels: ", 5, userInput);
 		userInput = change("Pennies: ", 1, userInput);
 	}
 	public static int change(String word, int value, int userInput) {
+		// method computes number of required coin and prints it to screen
+		if (userInput/value != 0) {
+		// if statement used to filter out blank results.
 		System.out.println(word + "" + (userInput/value));
-		return (userInput % value);
+		}
+		return (userInput % value); 
+		//remaining value returned for use in next calculation
 	}
 }
