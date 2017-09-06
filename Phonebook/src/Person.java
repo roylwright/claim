e9349a7ce1f8f212af14f6e802040615892db64c
RoyLWright <roylwright@gmail.com>
2017-09-06 16:32:35 -0500

    phone book works at minimum level

--- Phonebook/src/Person.java
+++ Phonebook/src/Person.java
@@ -1,31 +1,40 @@
 
 class Person {
-	public String name = "Roy Wright";
-	String address = "123 anystreet stl, MO, 63114";
-	public String telephone = "3144131177"; 
-	
+	private String name = "Roy Wright";
+	private String telephone = "3144131177"; 
+	private Address address;
 
    @Override
    public String toString() {
-        return ("\nName:"+this.getName()+
-                    " Phone No: "+ this.getTelephone());
+	   String line1 = "\nName: "+ getName();
+	   String line2 = "\n"+ getAddress();
+	   String line3 = "\nPhone No: (" + getTelephone().substring(0, 3) +")"+
+               getTelephone().substring(3,6)+"-"+getTelephone().substring(6);
+        return (line1 + line2 + line3);
    }
    public Person() {}
    public Person(String userInput, String telephone) {
 		this.name = userInput;
 		this.telephone = telephone;
 	}
-	private String getName() {
+
+   public String getName() {
 		return name;
 	}
-	private void setName(String name) {
+   public void setName(String name) {
 		this.name = name;
 	}
-	private String getTelephone() {
+   public String getTelephone() {
 		return telephone;
 	}
-	private void setTelephone(String telephone) {
+   public void setTelephone(String telephone) {
 		this.telephone = telephone;
 	}
+   public Address getAddress() {
+	   	return address;
+   	}
+   public void setAddress(Address address) {
+	   	this.address = address;
+   	}
 	
 }
--- Phonebook/src/Phonebook.java
+++ Phonebook/src/Phonebook.java
@@ -6,40 +6,87 @@ public class Phonebook {
 	static List<Person> directory = new ArrayList<Person>();
 	
 	public static void main(String[] args) {
+		String user = null;
+		testLoad(); //this line enters test data into array
 		
-		testLoad();
-		createDirectory();
-		//searchByFirstName();
-		searchByLastName();
-
-	}
+		do {
+			printMenu(); //prints menu options
+			String userInput = getInput(); //gets input and directs method to run
+			switch(userInput) {
+		    case "1":
+		    	createDirectory();
+		        break;
+		    case "2":
+		    	searchByFirstName();
+		    	break;
+		    case "3":
+		    	searchByLastName();
+		    	break;
+		    case "4":
+		    	searchByNumber();
+		    	break;
+		    case "5":
+		    	searchBycity();
+		    	break;	
+		    case "6":
+		    	searchByState();
+		    	break;	
+		    case "7":
+		    	deleteByNumber();
+		    	break;
+		    case "8":
+		    	deleteByNumber();
+		    	createDirectory();
+		    	break;
+		    case "0":
+		    	System.exit(0);
+			}
+		} while (user != "x");
 	
-	private static void testLoad() {
-		Person p1 = new Person();
-		p1.name = "John Doe";
-		p1.address = "114 Market St, St Louis, MO, 63403";
-		p1.telephone = "6366435698";
-		directory.add(p1);
-		Person p2 = new Person();
-		p2.name = "John E Doe";
-		p2.address = "324 Main St, St Charles, MO,63303";
-		p2.telephone = "8475390126";
-		directory.add(p2);
-		Person p3 = new Person();
-		p3.name = "John Michael West Doe";
-		p3.address = "574 Pole ave, St. Peters, MO, 63333";
-		p3.telephone = "5628592375";
-		directory.add(p3);
 		
+		System.out.println(directory.toString());
 	}
-	private static void searchByNumber() {
-		System.out.println("Enter phone number to search for: ");
+
+
+	private static void searchByState() {
+		System.out.println("Enter two digit state code to search for: ");
 		String userWord = getInput();
-		userWord = userWord;
 		String compare;
 		
 		for (int i=0; i<directory.size();i++) {
-			compare = directory.get(i).telephone;
+			compare = directory.get(i).getAddress().getState();
+			//System.out.println(compare);
+			if (compare.equals(userWord)) {
+				System.out.println(directory.get(i));
+			} 
+		}
+		
+	}
+
+
+	private static void searchBycity() {
+		System.out.println("Enter city to search for: ");
+		String userWord = getInput();
+		String compare;
+		
+		for (int i=0; i<directory.size();i++) {
+			compare = directory.get(i).getAddress().getCity();
+			//System.out.println(compare);
+			if (compare.equals(userWord)) {
+				System.out.println(directory.get(i));
+			} 
+		}
+		
+	}
+
+
+	private static void searchByNumber() {
+		System.out.println("Enter phone number to search for: ");
+		String userWord = getInput();
+		String compare;
+		
+		for (int i=0; i<directory.size();i++) {
+			compare = directory.get(i).getTelephone();
 			//System.out.println(compare);
 			if (compare.equals(userWord)) {
 				System.out.println(directory.get(i));
@@ -53,7 +100,7 @@ public class Phonebook {
 		String compare;
 		
 		for (int i=0; i<directory.size();i++) {
-			compare = directory.get(i).name.substring(0,userWord.length());
+			compare = directory.get(i).getName().substring(0,userWord.length());
 			//System.out.println(compare);
 			if (compare.equals(userWord)) {
 				System.out.println(directory.get(i));
@@ -66,7 +113,7 @@ public class Phonebook {
 		String userWord = getInput();
 		String compare;
 		for (int i=0; i<directory.size();i++) {
-			compare = directory.get(i).name.substring((directory.get(i).name.length()-userWord.length()));
+			compare = directory.get(i).getName().substring((directory.get(i).getName().length()-userWord.length()));
 			System.out.println(compare);
 			if (compare.equals(userWord)) {
 				System.out.println(directory.get(i));
@@ -80,9 +127,34 @@ public class Phonebook {
 		System.out.print("Enter telephone number: ");
 		String telephone = getInput();
 		Person p1 = new Person(name, telephone);
+		System.out.print("Enter street address: ");
+		String street = getInput();
+		System.out.print("Enter city: ");
+		String city = getInput();
+		System.out.print("Enter two letter state code: ");
+		String state = getInput();
+		System.out.print("Enter 5 digit zip code: ");
+		String zip = getInput();
+		Address a1 = new Address(street, city, state, zip);
+		p1.setAddress(a1);
 		directory.add(p1);
-		System.out.println("Size of directory after adds: " + directory.size()); 
-		System.out.println(directory.toString());				
+		
+		//System.out.println("Size of directory after adds: " + directory.size()); 
+		//System.out.println(directory.toString());				
+	}
+	private static void deleteByNumber() {
+		System.out.println("Enter phone number to search for: ");
+		String userWord = getInput();
+		String compare;
+		
+		for (int i=0; i<directory.size();i++) {
+			compare = directory.get(i).getTelephone();
+			//System.out.println(compare);
+			if (compare.equals(userWord)) {
+				System.out.println(directory.get(i));
+				directory.remove(i);
+			} 
+		}
 	}
 	public static String getInput() {
 		Scanner scanner = new Scanner(System.in);
@@ -90,5 +162,45 @@ public class Phonebook {
 		//scanner.close();
 		return userInput;
 	}
-
+	private static void printMenu() {
+		System.out.println("Enter character for your choice:");
+		System.out.println("1 for new entry:");
+		System.out.println("2 for search by first name:");
+		System.out.println("3 for search by last name:");
+		System.out.println("4 for search by phone number :");
+		System.out.println("5 for search by city:");
+		System.out.println("6 for search by state:");
+		System.out.println("7 for delete by phone number:");
+		System.out.println("8 for update by phone number:");
+		System.out.println("0 for exit program:");
+		//System.out.println(" for :"); //for later
+		
+	}
+	private static void testLoad() {
+		Person p1 = new Person();
+		Address a1 = new Address("114 Market St", "St Louis", "MO", "63403");
+		p1.setName("John Doe");
+		p1.setTelephone("6366435698");
+		p1.setAddress(a1);
+		directory.add(p1);
+		
+		Person p2 = new Person();
+		Address a2 = new Address("324 Main St", "St Charles", "MO","63303");
+		p2.setName("John E Doe");
+		p2.setAddress(a2);
+		p2.setTelephone("8475390126");
+		directory.add(p2);
+		
+		
+		Person p3 = new Person();
+		Address a3 = new Address("574 Pole ave", "St Peters", "MO", "63333");
+		p3.setName("John Michael West Doe");
+		p3.setAddress(a3);
+		p3.setTelephone("5628592375");
+		//p3.setTelephone("1234567890");
+		directory.add(p3);
+		
+		System.out.println("Size of directory after adds: " + directory.size()); 
+		System.out.println(directory.toString());
+	}
 }
